efce52d0d29df45478930ff6eba72efe5e2ac727
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-05 14:55:34 +0300

    Create TestAccount unittest class

---
+++ account_test.py
@@ -0,0 +1,16 @@
+import unittest
+from account import Account
+
+class TestAccount(unittest.TestCase):
+  '''
+  Test class that defines test cases for the account class behaviours.
+
+  Args:
+      unittest.TestCase: TestCase class that helps in creating test cases
+  '''
+
+
+
+if __name__ == '__main__':
+    unittest.main()           
+
