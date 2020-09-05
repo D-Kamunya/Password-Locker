9bb79ad2aab64e805abcc679d94a98f3a561a1ee
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-05 22:29:28 +0300

    Create testcredential subclass to test credential class behaviours

---
+++ credential_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestCredential(unittest.TestCase):
+    '''
+    Test class that defines test cases for the credential class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+
+
+if __name__ == '__main__':
+    unittest.main()           
+    
