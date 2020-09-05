d471d19d448b0c52cac76cb681e4d7ad30c6698d
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-05 22:26:54 +0300

    Create iinit method to initialize class attributes

--- credential.py
+++ credential.py
@@ -2,4 +2,13 @@ class Credential:
   """
   Class that generates an instance of a Credential
   """
-  pass
+  def __init__(self,page_name,pass_word):
+    '''
+    __init__ method that helps us define properties for our objects.
+
+    Args:
+        page_name:Name of page or account you want to save ctedentials fotrt
+        pass_word:Password of page you want to save
+    '''
+    self.page_name=page_name
+    self.pass_word=pass_word
