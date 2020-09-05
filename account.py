3f3d556b52a9e9eda3d5624bff34b0b02336ef2a
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-05 14:48:04 +0300

    Create constructor to allow  class initiate attribute

--- account.py
+++ account.py
@@ -2,4 +2,18 @@ class Account:
   """
   Class that generates an instance of an account
   """
-  pass 
+  def __init__(self,user_name,first_name,last_name,pass_word):
+    '''
+    __init__ method that helps us define properties for our objects.
+
+    Args:
+        user_name:Account user login user name
+        first_name: Account user login first name.
+        last_name : Account user login last name.
+        pass_word:Account user login password
+    '''
+    self.user_name=user_name
+    self.first_name=first_name
+    self.last_name=last_name
+    self.pass_word=pass_word
+      
