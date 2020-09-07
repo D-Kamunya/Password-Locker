cfae45ab64bd2c2c31d0de8ac96e943e94d02691
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-07 16:56:47 +0300

    Create while loop to run if user is not logged

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 from account import   Account
 from credential import Credential
 from termcolor import colored,cprint
+import os
+import time
 
 
 #Functions that implement the behaviours in account class.
@@ -87,7 +89,11 @@ def generate_password():
 
 
 def main():
+
+  login=False #Set initial login value to false 
+
   def display_title():
+    os.system('clear')
     '''
     Function to display app title bar
     '''
@@ -103,9 +109,65 @@ def main():
           \t\t\t\t       |_|_|I|_|_|
           \n\t\t\t\t***  GREETINGS USER, WELCOME TO PASSWORD LOCKER  ***
           \n`\t\t\t******************************************************************
-          ""","blue")
+          ""","magenta")
 
   display_title()
+
+
+  while login==False:
+    cprint("""
+      Use the following short codes to manage your password locker account 
+          'ln' - Login 
+          'xx' - Close app
+          ""","blue")
+    cprint("\tWhat would you like to do?",'cyan')
+    s_code = input().lower()
+    if s_code=='ln':
+      cprint('\tDo you have an account? Y/N','cyan')
+      acc_code=input().upper()
+      if acc_code=='Y':
+        cprint('\tEnter your username and password  to login >>>\n','blue')
+        login_user_name=input('\tEnter username >> ')
+        login_password=input('\tEnter password >> ')
+        print("\n\t\tSigning in...")
+        time.sleep(1.5)
+        if auth_user(login_user_name,login_password):
+          cprint('\n\t\tLOGIN SUCCESSFUL','green',attrs=['bold'])  
+          login=True
+        else:
+          cprint('\n\t\tSORRY COULD NOT VERIFY USER','red',attrs=['bold'])  
+
+      elif acc_code=='N':
+        cprint('\tEnter your username,firstname,lastname and password  to register account >>>\n','blue')
+        reg_user_name=input('\tEnter username >> ')
+        reg_f_name=input('\tEnter firstname >> ')
+        reg_l_name=input('\tEnter lastname >> ')
+        reg_password=input('\tEnter password >> ')
+        print("\n\t\tRegistering ...")
+        time.sleep(1.5)
+        new_acc=create_account(reg_user_name,reg_f_name,reg_l_name,reg_password)
+        save_account(new_acc)
+        cprint("\n\t\tCONGRATULATIONS, YOUR ACCOUNT HAS BEEN CREATED","green",attrs=['bold'])
+        cprint("\n\tSign into your new account","blue")
+        sign_username=input('\n\tEnter username >> ')
+        sign_password=input('\n\tEnter password >> ')
+        print("\n\t\tSigning in ...")
+        time.sleep(1.5)
+        if auth_user(sign_username,sign_password):
+          cprint("\n\t\tLOGIN SUCCESSFUL","green",attrs=['bold'])
+          login=True
+        else :
+          cprint('\n\t\tSORRY COULD NOT VERIFY USER','red',attrs=['bold'])  
+      else:
+        cprint('\n\t\tPLEASE USE THE GIVEN SHORT CODES','red',attrs=['bold'])  
+    elif s_code=='xx':
+      break
+    else:
+      cprint('\n\t\tPLEASE USE THE GIVEN SHORT CODES','red',attrs=['bold'])  
+
+  
+  while  login==True:
+    print('LOGGED IN')
 if __name__ == '__main__':
 
     main()    
